c398b61bcd5b49dd1f0ea3c0ef22febf7ec44ebc
BbayuGt <49893593+BbayuGt@users.noreply.github.com>
2026-05-03 15:05:35 +0700

    Menambahkan API Film

---
+++ TP_MODUL10_103002400085/Film.cs
@@ -0,0 +1,15 @@
+namespace TP_MODUL10_103002400085;
+
+public class Film
+{
+    public string Judul { get; set; }
+    public string Sutradara  { get; set; }
+    public string Tahun  { get; set; }
+    public string Genre   { get; set; }
+    public string Rating  { get; set; }
+
+    public Film()
+    {
+        
+    }
+}
--- TP_MODUL10_103002400085/Program.cs
+++ TP_MODUL10_103002400085/Program.cs
@@ -1,8 +1,12 @@
+using TP_MODUL10_103002400085;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -10,30 +14,75 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
-var summaries = new[]
+List<Film> films = new List<Film>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    new Film
+    {
+        Judul = "Inception",
+        Sutradara = "Christopher Nolan",
+        Tahun = "2010",
+        Genre = "Sci-Fi",
+        Rating = "9.0"
+    },
+    new Film
+    {
+        Judul = "Interstellar",
+        Sutradara = "Christopher Nolan",
+        Tahun = "2014",
+        Genre = "Sci-Fi",
+        Rating = "8.7"
+    },
+    new Film
+    {
+        Judul = "Parasite",
+        Sutradara = "Bong Joon-ho",
+        Tahun = "2019",
+        Genre = "Thriller",
+        Rating = "8.6"
+    }
 };
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/api/Film", () =>
     {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
+        return films;
     })
-    .WithName("GetWeatherForecast");
+    .WithName("/api/Film");
+
+app.MapPost("/api/Film", (Film newFilm) =>
+{
+    // validasi user
+    if (newFilm.Genre == null) return -1;
+    if (newFilm.Judul == null)  return -1;
+    if (newFilm.Rating == null) return -1;
+    if (newFilm.Sutradara == null) return -1;
+    if (newFilm.Tahun == null) return -1;
+    
+    films.Add(newFilm);
+    return films.Count; // return id film yang baru di post
+}).WithName("Add /api/Film");
+
+app.MapGet("/api/Film/{id}", (int id) =>
+{
+    if (id < 0 || id >= films.Count)
+    {
+        return null;
+    } // Kalau id tidak valid, return null
+
+    return films[id];
+}).WithName("/api/Film/{id}");
+
+app.MapDelete("/api/Film/{id}", (int id) =>
+{
+    if (id < 0 || id >= films.Count)
+    {
+        return -1;
+    } // check, kalau id tidak valid, return -1 (invalid)
+    films.RemoveAt(id);
+    return films.Count; // kembalikan jumlah film setelah penghapusan
+}).WithName("Delete /api/Film/{id}");
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
